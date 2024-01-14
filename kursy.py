9d832c857018032e8a349cb7930eb92c7bdbcb98
Kacper <kgrzesik17@gmail.com>
2024-01-14 13:52:19 +0100

    Automatyczne przeliczanie na API NBP

--- kursy.py
+++ kursy.py
@@ -1,6 +1,19 @@
 #autor: Kacper Grzesik
 import datetime
 import re
+import requests
+
+def przewalutowanie(kwota, waluta, data):
+    url = 'http://api.nbp.pl/api/exchangerates/tables/A/' + data
+
+    body = requests.get(url)
+    response = body.json()
+
+    for rate in response[0]['rates']:
+        if waluta == rate['code']:
+            wynik = float(kwota) * float(rate['mid'])
+            wynik = round(wynik, 2)
+            return wynik
 
 def kwota_walidacja(kwota):
     try:
@@ -12,7 +25,7 @@ def kwota_walidacja(kwota):
     return 1
 
 def waluta_walidacja(waluta):
-    dozwolone = ['pln', 'usd', 'eur', 'gbp']
+    dozwolone = ['PLN', 'USD', 'EUR', 'GBP']
 
     if waluta in dozwolone: return 1
     else:
@@ -40,7 +53,7 @@ def main():
 
     while True:
         fWaluta = input("Podaj walute [PLN/EUR/USD/GBP]: ")
-        fWaluta = fWaluta.lower() #przyjmowana jest dowolna wielkosc znakow
+        fWaluta = fWaluta.upper() #przyjmowana jest dowolna wielkosc znakow
 
         if waluta_walidacja(fWaluta): break
     
@@ -51,24 +64,37 @@ def main():
             print("\nPomyślnie wprowadzono dane faktury.")
             break
 
+    if fWaluta != "PLN":
+        fKwotaPrzed = fKwota
+        fKwota = przewalutowanie(fKwota, fWaluta, fData)
+        print(f"\nPrzekonwertowano {fKwotaPrzed} {fWaluta} na {fKwota} PLN.")
+
     #wprowadzanie danych platnosci
     print("\nPodaj dane platnosci\n")
 
     while True:
         pKwota = input("Podaj kwote: ")
+        pKwota = re.sub("\,", ".", pKwota) #zamiana ewentualnego przecinka na kropke
 
         if kwota_walidacja(pKwota): break
 
     while True:
         pWaluta = input("Podaj walute [PLN/EUR/USD/GBP]: ")
+        pWaluta = pWaluta.upper() #przyjmowana jest dowolna wielkosc znakow
 
         if waluta_walidacja(pWaluta): break
 
     while True:
         pData = input("Wprowadz date [rrrr-mm-dd]: ")
-        print("\nPomyślnie wprowadzono dane płatności.")
 
-        if data_walidacja(pData): break
+        if data_walidacja(pData):
+            print("\nPomyślnie wprowadzono dane faktury.")
+            break
+
+    if pWaluta != "PLN":
+        pKwotaPrzed = pKwota
+        pKwota = przewalutowanie(pKwota, pWaluta, pData)
+        print(f"\nPrzekonwertowano {pKwotaPrzed} {pWaluta} na {pKwota} PLN.")
 
 if __name__ == "__main__":
     main()
